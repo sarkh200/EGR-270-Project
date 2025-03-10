684f3b841fbf638d3e4bb54868f96a0f1246be54
sarkh200 <sarkh200@gmail.com>
2025-03-10 11:00:13 -0400

    initial commit

---
+++ drawFloorPlan.m
@@ -0,0 +1,12 @@
+
+
+map = [0 0 0;                   %black, inaccessible
+       1 0 0;                   %red, current robot location
+       0 0 1;                   %blue, charging station
+       127/255 50/255 50/255;   %brown, dirty floor
+       1 1 1;                   %white, clean floor
+       ];
+image(A) % display the contents of matrix A
+colormap(map) % Set the colormap to map
+axis('image') % resize the plot in the figure to the aspect ratio of the matrix
+axis off % turn off the axis labels
