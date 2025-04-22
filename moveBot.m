43492d19bd2b95f4640cd057755c335474e9019d
sarkh200 <sarkh200@gmail.com>
2025-04-22 13:12:30 -0400

    added a moveBot function and changed getFloorPlans to allow different sizes of floor plans

---
+++ moveBot.m
@@ -0,0 +1,31 @@
+function [matrix, robotLocation, newColorUnderBot] = moveBot(matrix, robotLocation, newRobotLocation, colorUnderBot, doesClean)
+    newColorUnderBot = colorUnderBot;
+
+    if robotLocation == newRobotLocation
+        return;
+    end
+
+    rows = size(matrix, 1);
+    columns = size(matrix, 2);
+
+    % make sure the location the robot is trying to go to is within the bounds of the map [columns rows]
+    %newRobotLocation(1) is y location ; newRobotLocation(2) is x location
+    if newRobotLocation(1) > 0 && newRobotLocation(1) <= rows && newRobotLocation(2) > 0 && newRobotLocation(2) <= columns && matrix(newRobotLocation(1), newRobotLocation(2)) ~= 0
+        % change the location under the robot back to what it was before robot went over it unless it's cleaning
+        if doesClean && colorUnderBot == 3
+            matrix(robotLocation(1), robotLocation(2)) = 4;
+        else
+            matrix(robotLocation(1), robotLocation(2)) = colorUnderBot;
+        end
+
+        %change colorUnderRobot to where the robot is going to go to
+        newColorUnderBot = matrix(newRobotLocation(1), newRobotLocation(2));
+        %newColorUnderBot = 5;
+
+        %change the location of the robot in the map to the new location
+        matrix(newRobotLocation(1), newRobotLocation(2)) = 1;
+        % change the location of the robot to where is wants to go to
+        robotLocation = newRobotLocation;
+    end
+
+end
--- test.m
+++ test.m
@@ -1,5 +1,5 @@
 [model, plan] = main_menu();
-floorMap = getFloorPlans(plan);
+floorMap = getFloorPlans(plan{1});
 
 drawFloorPlan(floorMap);
 
@@ -11,6 +11,8 @@ userInput = '';
 robotLocation = [2, 2]; %the starting location of our roomba
 
 colorUnderRobot = floorMap(robotLocation(1), robotLocation(2)); %color under initial location (bound to change after loop)
+
+doClean = false;
 
 while strcmp(userInput, 'end') == false %Pseudocode practice: direct input of movement (WASD)
     userInput = input('>', 's');
@@ -25,17 +27,23 @@ while strcmp(userInput, 'end') == false %Pseudocode practice: direct input of mo
             newRobotLocation(2) = robotLocation(2) - 1;
         case 'd'
             newRobotLocation(2) = robotLocation(2) + 1;
+        case 'c'
+            doClean = true;
+        case 'm'
+            doClean = false;
     end
 
-    % make sure the location the robot is trying to go to is within the bounds of the map [25 30]
-    %newRobotLocation(1) is y location ; newRobotLocation(2) is x location
-    if (newRobotLocation(1) > 0 && newRobotLocation(1) < 31 && newRobotLocation(2) > 0 && newRobotLocation(2) < 26)
-        floorMap(robotLocation(1), robotLocation(2)) = colorUnderRobot; % change the location under the robot back to what it was before robot went over it
-        %colorUnderRobot = floorMap(newRobotLocation(1), newRobotLocation(2)); %change colorUnderRobot to where the robot is going to go to
-        colorUnderRobot = 5;
-        floorMap(newRobotLocation(1), newRobotLocation(2)) = 1; %change the location of the robot in the map to the new location
-        robotLocation = newRobotLocation; % change the location of the robot to where is wants to go to
-    end
+    [floorMap, robotLocation, colorUnderRobot] = moveBot(floorMap, robotLocation, newRobotLocation, colorUnderRobot, doClean);
+
+    % % make sure the location the robot is trying to go to is within the bounds of the map [25 30]
+    % %newRobotLocation(1) is y location ; newRobotLocation(2) is x location
+    % if (newRobotLocation(1) > 0 && newRobotLocation(1) < 31 && newRobotLocation(2) > 0 && newRobotLocation(2) < 26)
+    %     floorMap(robotLocation(1), robotLocation(2)) = colorUnderRobot; % change the location under the robot back to what it was before robot went over it
+    %     %colorUnderRobot = floorMap(newRobotLocation(1), newRobotLocation(2)); %change colorUnderRobot to where the robot is going to go to
+    %     colorUnderRobot = 5;
+    %     floorMap(newRobotLocation(1), newRobotLocation(2)) = 1; %change the location of the robot in the map to the new location
+    %     robotLocation = newRobotLocation; % change the location of the robot to where is wants to go to
+    % end
 
     clc();
     drawFloorPlan(floorMap);
