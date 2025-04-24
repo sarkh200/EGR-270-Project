f603e6e2e018a7e3d57ec4d257195cd653e135b6
Sadra Saharkhiz <sarkh200@gmail.com>
2025-04-24 01:33:37 -0400

    test a* is done

--- testAStar.m
+++ testAStar.m
@@ -18,22 +18,20 @@ matrix=[
         0 4 0 0 0 0 0 0 0 0
         0 4 4 4 4 4 4 4 4 0
         0 0 0 0 0 0 0 0 4 0
-        0 4 4 4 4 4 4 4 3 0
+        0 5 4 4 4 4 4 4 4 0
         0 0 0 0 0 0 0 0 0 0
         ];
 
 [r, c]=find(matrix==2);
 startLocation=[r, c];
 
-[r, c]=find(matrix==3);
-endLocation=[r-1, c];
+[r, c]=find(matrix==5);
+endLocation=[r, c];
 
 path=aStarPath(matrix, startLocation, endLocation);
 
 [matrix, currentLocation, colorUnderBot]=moveAlongPath(matrix, path, 2, 100);
 
-[matrix, currentLocation, colorUnderBot, batteryLife, ~]=moveBot(matrix, currentLocation, [r, c], colorUnderBot, batteryLife, true, 1);
-
 path=aStarPath(matrix, currentLocation, startLocation);
 
-[~, ~, ~]=moveAlongPath(matrix, path, colorUnderBot, 100);
+[~, ~, ~]=moveAlongPath(matrix, path, 4, 100);
