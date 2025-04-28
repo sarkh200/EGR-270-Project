3090016c61454e61692bc21c69ab9999412db060
halitsonmez <127158497+halitsonmez@users.noreply.github.com>
2025-04-28 09:03:01 -0400

    more coms

--- highend_CSSP.m
+++ highend_CSSP.m
@@ -1,7 +1,7 @@
-function [total_area, ratio_clean_to_dirty, cleaned_squares]=highend_CSSP(floorMap)
+function [total_area, ratio_clean_to_dirty, cleaned_squares]=highend_CSSP(floorMap) %Simulates the high end cleaning robot
     global batteryLife;
-    [y, Fs]=audioread('sounds/epic_music.mp3');
-    sound(y, Fs, 16);
-    batteryLife=250;
-    [total_area, ratio_clean_to_dirty, cleaned_squares]=scottbot(floorMap, [3, 3], batteryLife);
+    [y, Fs]=audioread('sounds/epic_music.mp3'); %Plays epic music
+    sound(y, Fs, 16); 
+    batteryLife=250; %Sets battery life
+    [total_area, ratio_clean_to_dirty, cleaned_squares]=scottbot(floorMap, [3, 3], batteryLife); %Pulls floormap, total area, clean, dirty squares
 end
--- regular_CSSP.m
+++ regular_CSSP.m
@@ -1,7 +1,7 @@
-function [total_area, ratio_clean_to_dirty, cleaned_squares]=regular_CSSP(floorMap)
+function [total_area, ratio_clean_to_dirty, cleaned_squares]=regular_CSSP(floorMap) %Simulates the cleaning routine with the regular  model
     global batteryLife;
-    [y, Fs]=audioread('sounds/classical.mp3');
+    [y, Fs]=audioread('sounds/classical.mp3'); %Plays classical music
     sound(y, Fs, 16);
-    batteryLife=350;
-    [total_area, ratio_clean_to_dirty, cleaned_squares]=scottbot(floorMap, [3, 4], batteryLife);
+    batteryLife=350; %Sets battery life
+    [total_area, ratio_clean_to_dirty, cleaned_squares]=scottbot(floorMap, [3, 4], batteryLife); %Pulls floormap, total area, clean, and dirty squares
 end
