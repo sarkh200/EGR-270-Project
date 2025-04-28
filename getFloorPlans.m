6a0cc1d760124dc1e7bc3373ba397c228b3e6523
halitsonmez <127158497+halitsonmez@users.noreply.github.com>
2025-04-28 08:26:35 -0400

    comments

--- getFloorPlans.m
+++ getFloorPlans.m
@@ -1,7 +1,7 @@
 function floorMap = getFloorPlans(inFileName)
-    inFid = fopen(inFileName);
-    columns = length(erase(fgetl(inFid), " "));
-    fseek(inFid, 0, 'bof');
-    floorMap = fscanf(inFid, '%i', [columns, Inf])';
-    fclose(inFid);
+    inFid = fopen(inFileName); %Open the input file for reading
+    columns = length(erase(fgetl(inFid), " ")); %Read the first line and remove spaces to determine the number of columns
+    fseek(inFid, 0, 'bof'); %Reset the file position indicator back to the beginning of the file
+    floorMap = fscanf(inFid, '%i', [columns, Inf])'; %Read the file as integers into a 2D array
+    fclose(inFid); %Close the file
 end
