7d5a05e0c3f03856307219ab77952d98503c0636
halitsonmez <127158497+halitsonmez@users.noreply.github.com>
2025-04-28 08:38:55 -0400

    more comments

--- distanceBetween.m
+++ distanceBetween.m
@@ -1,3 +1,4 @@
+%Finds the distance between start and end position
 function distance=distanceBetween(startPosition, endPosition)
     distance=max(abs(startPosition(1)-endPosition(1)), abs(startPosition(2)-endPosition(2)));
 end
