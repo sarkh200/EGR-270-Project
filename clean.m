cffcfb64e27bced68ae08f6b2cb8318f9f27b356
sarkh200 <sarkh200@gmail.com>
2025-04-28 08:39:30 -0400

    added comments

--- clean.m
+++ clean.m
@@ -1,11 +1,18 @@
 function [matrix, colorUnderBot]=clean(matrix, robotPosition)
+    % clean cleans the current tile the robot is on
+    % Inputs:
+    %   matrix = the floor plan (in the form of a matrix)
+    %   robotPosition = the position of the robot (in the form of [row, column])
+    % Outputs:
+    %   matrix = the updated floor plan (in the form of a matrix)
+    %   colorUnderBot = a number that represents what color the robot is currently standing on
     global time batteryLife;
-    
-    colorUnderBot = matrix(robotPosition(1), robotPosition(2));
-    if colorUnderBot ~= 2
+
+    colorUnderBot=matrix(robotPosition(1), robotPosition(2));
+    if colorUnderBot~=2
         matrix(robotPosition(1), robotPosition(2))=5;
         batteryLife=batteryLife-1;
         colorUnderBot=5;
-        time = time + 5;
+        time=time+5;
     end
 end
