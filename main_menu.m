7c0add68803ef2fa51248c9e3c638c1a2d1d88e0
noahbenchill <noah.b.childers@gmail.com>
2025-04-28 08:38:20 -0400

    some h1 lines

--- main_menu.m
+++ main_menu.m
@@ -1,4 +1,9 @@
 function [choice_model, choice_floor] = main_menu()
+    % Input Arguments:
+    % None
+    % Output Arguments:
+    % Choice_model = User input for the roomba model
+    % Choice_floor = User input for the floor plan
     prompt_model = "Which Roomba model are you using?";
     model_options = ["Economy", "Regular", "High End"];
     choice_model = menu(prompt_model, model_options);
