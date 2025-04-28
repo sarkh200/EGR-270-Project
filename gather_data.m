818e792305002f5ee0115b258b6098abeed9e042
halitsonmez <127158497+halitsonmez@users.noreply.github.com>
2025-04-28 09:12:10 -0400

    comm

--- gather_data.m
+++ gather_data.m
@@ -1,3 +1,8 @@
+%gather_data gathers the selected floormap data like total area, dirty squares, and clean to dirty squares ratio
+% Input arguments:
+% matrix = floor plan
+% Output arguments:
+% Total area, dirty squares, and clean to dirty squares ratio
 function [total_area, ratio_clean_to_dirty] = gather_data(matrix)
     total_area = length(find(matrix~=0)); %total squares excluding the walls
     dirty_squares = length(find(matrix==3)); %total dirty squares
